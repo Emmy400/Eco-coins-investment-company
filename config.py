8a4a26446b84b4cb96afd3926b7f5d1842b65f52
Emmy400 <olumideolamide810@gmail.com>
2026-03-31 12:52:17 +0100

    Create config.py with Flask configuration settings

---
+++ config.py
@@ -0,0 +1,9 @@
+# Flask Configuration
+
+import os
+
+class Config:
+    """Set Flask configuration from environment variables."""
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default_secret_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
